dc4ec1ec006d8c50383e753de163e240028ae70b
DSegebarth <d.segebarth@gmail.com>
2022-04-21 17:45:05 +0200

    wip - initial refactored build with one sample comparison as example almost done, only GUI missing

--- dcl_stats_n_plots/_nbdev.py
+++ dcl_stats_n_plots/_nbdev.py
@@ -5,42 +5,12 @@ __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 index = {"independent_samples": "00_stats.ipynb",
          "one_sample": "00_stats.ipynb",
          "mixed_model_ANOVA": "00_stats.ipynb",
-         "DEFAULT_PARAMS": "01_plots.ipynb",
-         "l_required_keys_ind_samples": "01_plots.ipynb",
-         "l_required_keys_one_sample": "01_plots.ipynb",
-         "l_required_keys_mma": "01_plots.ipynb",
-         "initialize_plot": "01_plots.ipynb",
-         "finish_show_and_save_plot": "01_plots.ipynb",
-         "get_stars_str": "01_plots.ipynb",
-         "sort_by_third": "01_plots.ipynb",
-         "plot_independent_samples": "01_plots.ipynb",
-         "annotate_stats_independent_samples": "01_plots.ipynb",
-         "plot_one_sample": "01_plots.ipynb",
-         "annotate_stats_one_sample": "01_plots.ipynb",
-         "plot_mma": "01_plots.ipynb",
-         "annotate_stats_mma": "01_plots.ipynb",
-         "annotate_stats_mma_pointplot": "01_plots.ipynb",
-         "STYLE": "03_dashboards.ipynb",
-         "INITIAL_WIDGET_PARAMS": "03_dashboards.ipynb",
-         "Gui": "02_gui.ipynb",
-         "Select_stats_widget": "02_gui.ipynb",
-         "Select_plots_widget": "02_gui.ipynb",
-         "Select_downloads_widget": "02_gui.ipynb",
-         "Customization_widget": "02_gui.ipynb",
-         "Customize_annotations": "02_gui.ipynb",
-         "Select_annotations": "02_gui.ipynb",
-         "Customize_y_axis": "02_gui.ipynb",
-         "Customize_x_axis": "02_gui.ipynb",
-         "Customize_both_axes": "02_gui.ipynb",
-         "Customize_other_features": "02_gui.ipynb",
-         "launch": "03_dashboards.ipynb",
-         "Dashboards": "03_dashboards.ipynb",
-         "Alexia_patched_cells_vs_baseline": "03_dashboards.ipynb"}
+         "DEFAULT_CONFIGS": "00r_database.ipynb",
+         "Configs": "00r_database.ipynb",
+         "Database": "00r_database.ipynb"}
 
 modules = ["stats.py",
-           "plots.py",
-           "gui.py",
-           "dashboards.py"]
+           "database.py"]
 
 doc_url = "https://DSegebarth.github.io/dcl_stats_n_plots/"
 
